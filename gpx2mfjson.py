0f0f7ee5854086e33145b90568ec1c9e3f81d9b6
nob140 <nobu140@gmail.com>
2023-09-30 17:48:10 +0900

    Update gpx2mfjson.py
    bug fixed

--- gpx2mfjson.py
+++ gpx2mfjson.py
@@ -16,28 +16,30 @@ def gpx2mfjson2(inputfile, outputfile):
     gpx = gpxpy.parse(gpx_file)
 
     features = []
-    num = 0
+    tracknum = 0
 
     for track in gpx.tracks:
+        tracknum += 1
+
+        # making lists without invalid points
         coordinates = []
         times = []
-        num += 1
-
+        lasttime = "0000-00-00"
         for segment in track.segments:
             for point in segment.points:
-                if point.latitude is None or point.longitude is None or point.elevation is None:
-                    print('invalid point')
+                if point.latitude is None or point.longitude is None or point.elevation is None or point.time is None:
+                    #print('invalid point')
                     continue
-                if point.time is None:
-                    print('invalid timestamp')
-                    continue
+                
+                # convert time to timestr
+                point.time = point.time.strftime('%Y-%m-%dT%H:%M:%S%z')
+                # save only unique timestamp
+                if lasttime != point.time:
+                    coordinates.append([point.longitude, point.latitude, point.elevation])
+                    times.append(point.time)
+                lasttime = point.time
 
-                coordinates.append([point.longitude, point.latitude, point.elevation])
-
-                dtime = point.time
-                times.append(dtime.strftime('%Y-%m-%dT%H:%M:%S%z'))
-    
-        feature = Feature(id='Route'+str(num), geometry= LineString(coordinates= coordinates, precision=15), properties= {'datetime':times})
+        feature = Feature(id= f"Route{tracknum}", geometry= LineString(coordinates= coordinates, precision=15), properties= {'datetimes':times})
         features.append(feature)
 
     gpx_file.close()
@@ -48,6 +50,7 @@ def gpx2mfjson2(inputfile, outputfile):
 
 # converter function considering epsilon
 # delete middle point of 3 same points by checking 'distance < epsilon'
+# epsilon 0.00000001 is mm order
 def gpx2mfjson3(inputfile, outputfile, epsilon):
     gpx_file = open(inputfile, 'r')
     gpx = gpxpy.parse(gpx_file)
@@ -59,39 +62,47 @@ def gpx2mfjson3(inputfile, outputfile, epsilon):
         tracknum += 1
 
         # making lists without invalid points
-        lats, lons, elevs, times = [], [], [], []
+        points = []
+        lasttime = "0000-00-00"
         for segment in track.segments:
             for point in segment.points:
                 if point.latitude is None or point.longitude is None or point.elevation is None or point.time is None:
                     #print('invalid point')
                     continue
-                lats.append(point.latitude)
-                lons.append(point.longitude)
-                elevs.append(point.elevation)
-                times.append(point.time)
+
+                # convert time to timestr
+                point.time = point.time.strftime('%Y-%m-%dT%H:%M:%S%z')
+                # save only unique timestamp
+                if lasttime != point.time:
+                    points.append(point)
+                lasttime = point.time
 
         # check same 3 points
-        for index in range(len(lats)-2):
-            # check horizontal distance
-            if abs(lats[index] - lats[index+1]) > epsilon or abs(lats[index+1] - lats[index+2]) > epsilon:
+        for i in range(len(points)-2):
+            # check horizontal distance (do not mind unit of measure)
+            if abs(points[i].latitude - points[i+1].latitude) > epsilon:
                 continue
-            if abs(lons[index] - lons[index+1]) > epsilon or abs(lons[index+1] - lons[index+2]) > epsilon:
+            if abs(points[i+1].latitude - points[i+2].latitude) > epsilon:
+                continue
+            if abs(points[i].longitude - points[i+1].longitude) > epsilon:
+                continue
+            if abs(points[i+1].longitude - points[i+2].longitude) > epsilon:
                 continue
 
             # unnecessary point flag
-            times[index+1] = None
-        
+            points[i+1].time = None
+
         # feature by linestring geometry with only valid points
         coordinates = []
-        timestrs = []
-        for index in range(len(lats)):
-            if times[index] is None:
+        times = []
+        for point in points:
+            if point.time is None:
                 continue
 
-            coordinates.append([lons[index], lats[index], elevs[index]])
-            timestrs.append(times[index].strftime('%Y-%m-%dT%H:%M:%S%z'))
+            coordinates.append([point.longitude, point.latitude, point.elevation])
+            times.append(point.time)
     
-        feature = Feature(id='Route'+str(tracknum), geometry= LineString(coordinates= coordinates, precision=15), properties= {'datetime':timestrs})
+        feature = Feature(id= f"Route{tracknum}", geometry= LineString(coordinates= coordinates, precision=15), properties= {'datetimes':times})
         features.append(feature)
 
     gpx_file.close()
@@ -119,3 +130,5 @@ if __name__ == '__main__':
         gpx2mfjson2(inputfile, arguments[2])
     else:
         gpx2mfjson3(inputfile, arguments[2], float(arguments[3]))
+
+    print('process finished successfully.')
